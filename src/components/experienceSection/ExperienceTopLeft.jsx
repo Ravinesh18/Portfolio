5a089241fa55ea7b4ac29cd1dcf4e59cdb595618
Ravinesh18 <mishravinesh18@gmail.com>
2025-03-14 11:51:56 +0530

    na

--- src/components/experienceSection/ExperienceTopLeft.jsx
+++ src/components/experienceSection/ExperienceTopLeft.jsx
@@ -7,15 +7,15 @@ const ExperienceTopLeft = () => {
         Since 2023
       </p>
       <div className="flex justify-center items-center gap-4">
-        <ExperienceInfo number="3" text="Years" />
+        <ExperienceInfo number="2" text="Years" />
         <p className="font-bold text-6xl text-lightBrown">-</p>
-        <ExperienceInfo number="23" text="Websites" />
+        <ExperienceInfo number="15" text="Websites" />
       </div>
       <p className="text-center">
         With 2 years of experience building dynamic and user-friendly web
         applications.
       </p>
-      <ExperienceInfo number="$100k" text="Max Budget" />
+      {/* <ExperienceInfo number="$100k" text="Max Budget" /> */}
     </div>
   );
 };
